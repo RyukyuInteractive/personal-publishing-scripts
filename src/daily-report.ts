bbba93379ef787db41a1eb2fd49535c220134f3f
atomita <sleeping.cait.sith+gh@gmail.com>
2020-01-10 17:38:56 +0900

    feat: Add daily-report

---
+++ src/daily-report.ts
@@ -0,0 +1,34 @@
+[].map.call(document.getElementsByTagName("table"), table => {
+  try {
+    const task = table.children[0].children[0].children[0].textContent;
+    if ("task" === task) {
+      return [].map.call(table.children[1].children, record => {
+        const spend = record.children[2];
+        const text = spend.childNodes[0];
+
+        const times = text.nodeValue;
+        if (times && /\d+:\d+(:\d+)?(\s*\+\s*\d+:\d+(:\d+)?)*/.test(times)) {
+          spend.innerText = [
+            times
+              .split(/\s*\+\s*/)
+              .map(v =>
+                [v.split(":")].reduce(
+                  (_, [h, m = 0, s = 0]) => h * 60 ** 2 + m * 60 + s,
+                  0
+                )
+              )
+              .reduce((acc, v) => acc + v, 0)
+          ].reduce(
+            (_, v) =>
+              Math.floor(v / 60 ** 2) +
+              ":" +
+              ("0" + (v % 60 ** 2) / 60).slice(-2) +
+              ":" +
+              ("0" + ((v % 60 ** 2) % 60)).slice(-2),
+            0
+          );
+        }
+      });
+    }
+  } catch (e) {}
+});
