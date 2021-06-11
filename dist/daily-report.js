29fb214f2859c472bbc275a2873f32d7325df5cd
atomita <sleeping.cait.sith+gh@gmail.com>
2021-06-11 10:31:27 +0900

    yarn upgrade; yarn build

--- dist/daily-report.js
+++ dist/daily-report.js
@@ -7,25 +7,34 @@
       return [].map.call(table.children[1].children, function (record) {
         var spend = record.children[2];
         var text = spend.childNodes[0];
-        
+
         var times = text.nodeValue;
         if (times && /\d+:\d+(:\d+)?(\s*\+\s*\d+:\d+(:\d+)?)*/.test(times)) {
           spend.innerText = [
-            times.split(/\s*\+\s*/).map(
-              function (v) { return [
-                v.split(":")
-              ].reduce(function (_, ref) {
-                var h = ref[0];
-                var m = ref[1]; if ( m === void 0 ) m = 0;
-                var s = ref[2]; if ( s === void 0 ) s = 0;
+            times
+              .split(/\s*\+\s*/)
+              .map(function (v) { return [v.split(":")].reduce(
+                  function (_, ref) {
+                    var h = ref[0];
+                    var m = ref[1]; if ( m === void 0 ) m = 0;
+                    var s = ref[2]; if ( s === void 0 ) s = 0;
 
-                return h * Math.pow( 60, 2 ) + m * 60 + s;
-                }, 0); }
-            )
-            .reduce(function (acc, v) { return acc + v; }, 0)
-          ].reduce(function (_, v) { return Math.floor(v / Math.pow( 60, 2 )) + ":" + ("0" + v % Math.pow( 60, 2 ) / 60).slice(-2) + ":" + ("0" + (v % Math.pow( 60, 2 )) % 60).slice(-2); }, 0);
+                    return h * Math.pow( 60, 2 ) + m * 60 + s;
+                  },
+                  0
+                ); }
+              )
+              .reduce(function (acc, v) { return acc + v; }, 0)
+          ].reduce(
+            function (_, v) { return Math.floor(v / Math.pow( 60, 2 )) +
+              ":" +
+              ("0" + (v % Math.pow( 60, 2 )) / 60).slice(-2) +
+              ":" +
+              ("0" + ((v % Math.pow( 60, 2 )) % 60)).slice(-2); },
+            0
+          );
         }
-      })
+      });
     }
   } catch (e) {}
 });
